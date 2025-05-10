23689c1df331c8a84b74e467131df40a08160972
thsouzAc <thiagosouzabrito8@gmail.com>
2025-05-10 07:59:49 -0300

    Coletando Resultados

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import os
 from functions.metrics import calcular_metricas
 from functions.results import results
 
-
 def main():
     
     n = 35
-    lista_n = [3000, 3200, 3400, 3600, 3800, 4000, 4200, 4400]
+    lista_n = [300000, 320000, 340000, 360000, 380000, 400000, 420000, 440000]
     num_threads = os.cpu_count() or 4
     results(n, lista_n ,num_threads)
 
